f2453d18164562664eddccaf17f854ffefa1ee65
Ronald Roberts <djamadeus1@gmail.com>
2024-10-30 14:58:07 -0400

    oh boy

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
-import About from "./About";
-import ArticleList from "./ArticleList";
+import About from "./about";
+import ArticleList from "./Articlelist";
 
 console.log(blogData);
 
